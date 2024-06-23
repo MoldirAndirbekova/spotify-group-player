30ea9a7dab4ade291c075996cada6aa8f5e6c594
Moldir <moldir.andirbekova@mail.ru>
2024-06-23 18:50:13 +0500

    created room model, serilizer and api endpoint

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serilizers import RoomSerializer
 
-def main(request):
-    return HttpResponse("Hello world")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
